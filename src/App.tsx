cfc8d165ceb6f042f21fed13078534e8b874a293
Alonso Astroza Tagle <alonsoastroza@gmail.com>
2026-07-09 00:59:17 -0400

    Polish sidebar contact icons

--- src/App.tsx
+++ src/App.tsx
@@ -1414,8 +1414,8 @@ function MailIcon() {
 
 function XIcon() {
   return (
-    <svg viewBox="0 0 24 24" aria-hidden="true">
-      <path d="M4 4l16 16M20 4 4 20" />
+    <svg className="x-logo" viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M18.24 2.25h3.31l-7.23 8.26 8.5 11.24h-6.66l-5.21-6.82-5.97 6.82H1.67l7.73-8.84L1.23 2.25h6.83l4.71 6.23 5.47-6.23Zm-1.16 17.52h1.83L7.07 4.13H5.1l11.98 15.64Z" />
     </svg>
   );
 }
